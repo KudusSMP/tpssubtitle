acb7aa13178a863ebed1a848b5f86af7493036a3
KudusSMP <vanuckinaleksej@gmail.com>
2026-06-28 22:05:19 +0300

    Add files via upload

---
+++ AuthImport_GitHubReady/src/main/java/me/authimport/AuthImportPlugin.java
@@ -0,0 +1,10 @@
+package me.authimport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuthImportPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("AuthImport enabled");
+    }
+}
